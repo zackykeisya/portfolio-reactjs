27e63380573f1b22f4110daf970815b34ee5066e
Zacky Keisya <zackykeisya@Zackys-MacBook-Pro.local>
2025-11-26 17:27:59 +0700

    add more style

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -46,7 +46,7 @@ const Hero = () => {
                   className="hero-btn-primary"
                   target="_blank" 
                   rel="noopener noreferrer"
-                  download="cv-zacky-keisya"
+                  download="cv-zackykeisyaakbar.pdf"
                 >
                   <svg className="w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24">
                     <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M12 10v6m0 0l-3-3m3 3l3-3m2 8H7a2 2 0 01-2-2V5a2 2 0 012-2h5.586a1 1 0 01.707.293l5.414 5.414a1 1 0 01.293.707V19a2 2 0 01-2 2z" />
